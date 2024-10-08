3a43251d3394d6b0b5017349b14e1f43fab687ce
MatteoDiMario96 <dm.matteo96@live.it>
2024-10-08 17:54:39 +0200

    Aggiunti commenti e pulizia generale.

--- app/Http/Controllers/Admin/ChefController.php
+++ app/Http/Controllers/Admin/ChefController.php
@@ -7,62 +7,79 @@ use App\Http\Requests\StoreChefRequest;
 use App\Http\Requests\UpdateChefRequest;
 use App\Models\Chef;
 use App\Models\Specialization;
-use App\Models\User;
-use App\Models\Message;
-use App\Models\Review;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class ChefController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display una lista della risorsa
      */
     public function index()
     {
+        /**
+         * Prendi l'utente attualmete autenticato
+         */
         $user = auth()->user();
-        if (!$user) {
 
+        /**
+         * Se non lo è ritorna nella pagina login con messaggio key:not-auth
+         */
+        if (!$user) {
             return redirect()->route('login')->with('not-auth', "Devi aver effettutato l'accesso per visualizzare questa pagina.");
         }
+
+        /**
+         * Prendi tutti gli chefs
+         */
         $chefs = Chef::all();
 
-
-
+        /**
+         * Ritorna la view index con chiave e valore di chefs grazie a compact
+         */
         return view('admin.chefs.index', compact('chefs'));
     }
 
+
+
     /**
-     * Show the form for creating a new resource.
+     * Per creare una nuova risorsa
      */
     public function create()
     {
-        //
+        /**
+         * Prendi l'utente attualmete autenticato
+         */
         $user = auth()->user();
-        if (!$user) {
 
+
+        /**
+         * Se non lo è ritorna nella pagina login con messaggio key:not-auth
+         */
+        if (!$user) {
             return redirect()->route('login')->with('not-auth', "Devi aver effettutato l'accesso per visualizzare questa pagina.");
         }
 
-        $chefs = new Chef();
+        $chef = new Chef();
         $specializations = Specialization::all();
-
-
-
-
-        return view('admin.chefs.create', compact('chefs', 'specializations'));
+        return view('admin.chefs.create', compact('chef', 'specializations'));
     }
 
     /**
-     * Store a newly created resource in storage.
+     * * Stora la nuova risorsa creata
      */
     public function store(StoreChefRequest $request)
     {
-
-
-
+        /**
+         * Prendi i dati validati della request
+         */
         $data = $request->validated();
+
+
+        /**
+         * Se la richiesta ha un file storala in public nel percorso...
+         *
+         */
         if ($request->hasFile('photograph')) {
             $img_path = Storage::disk('public')->put('upload/img', $data['photograph']);
             $data["photograph"] = $img_path;
@@ -72,22 +89,43 @@ class ChefController extends Controller
             $data["CV"] = $file_path;
         }
 
+        // Questo per associare lo user_id a l'id dell'utente autenticato
         $data['user_id'] = Auth::id();
+
+
+        /**
+         * Crea uno chef con le fillable
+         *
+         * facciamo in modo che
+         */
         $newChef = Chef::create($data);
+
+        /**
+         * Associa la relazione specializations a newChef e ci aggiunge o toglie le specializzazioni della request
+         */
         $newChef->specializations()->sync($data['specializations']);
         return redirect()->route('admin.chefs.show', $newChef)->with('create-chef', $newChef->user->name . ' ' . 'has been CREATE with success');
     }
 
     /**
-     * Display the specified resource.
+     * Mostra una specifica risorsa
      */
     public function show(Chef $chef)
     {
-        //$user = auth()->user();
+        /**
+         * Controlla se l'utente è autenticato
+         */
         if (!Auth::check()) {
-
             return redirect()->route('login')->with('not-auth', "Devi aver effettutato l'accesso per visualizzare questa pagina.");
         }
+
+        /**
+         * Se l'id dell'utente autenticato è uguale all attributo user_id di chef
+         *
+         *    se si rirtorna la view show
+         *
+         *    altrimenti ritorna la dashboard con messaggio wrong-user
+         */
         if (Auth::id() === $chef->user_id) {
             return view('admin.chefs.show', compact('chef'));
         } else {
@@ -95,23 +133,31 @@ class ChefController extends Controller
         }
     }
 
-
+    /**
+     * Metodo per tornare view per lo user autenticato
+     */
     public function userAuthenticated($user_id)
     {
-
         return view('admin.chefs.show', compact('user_id'));
     }
 
+
+    /**
+     * Metodo per controllo autenticazione per vedere la dashboard
+     */
     public function viewDashboard()
     {
         $user = auth()->user();
         if (!$user) {
-
             return redirect()->route('login')->with('not-auth', "Devi aver effettutato l'accesso per visualizzare questa pagina.");
         }
         return view('admin.dashboard');
     }
 
+
+    /**
+     * Metodo per controllo autenticazione per vedere i messaggi nella dashboard
+     */
     public function viewMessage(Chef $chef)
     {
         $user = auth()->user();
@@ -126,6 +172,10 @@ class ChefController extends Controller
         return view('admin.chefs.profile.message', compact('chef'));
     }
 
+
+    /**
+     * Metodo per controllo autenticazione per vedere le recensioni nella dashboard
+     */
     public function viewReview(Chef $chef)
     {
         $user = auth()->user();
@@ -141,34 +191,43 @@ class ChefController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Mostra il form per la modifica se l'utente è autenticato
      */
     public function edit(Chef $chef)
     {
         $user = auth()->user();
-        if (!$user) {
 
+        if (!$user) {
             return redirect()->route('login')->with('not-auth', "Devi aver effettutato l'accesso per visualizzare questa pagina.");
         }
-        //
+
         $specializations = Specialization::all();
+
         return view('admin.chefs.edit', compact('chef', 'specializations'));
     }
 
     /**
-     * Update the specified resource in storage.
+     * Aggiorna la risorsa specifica nello store
      */
 
     public function update(UpdateChefRequest $request, Chef $chef)
     {
+        /**
+         * prendi la request validata
+         */
         $data = $request->validated();
 
-        // Se nella request hai il file 'photograph' manda avanti la modifica. Altrimenti non fare nulla.
+        /**
+         * Se nella request hai il file 'photograph' storala nel disco con name public e mettilo nel percorso....
+         */
         if ($request->hasFile('photograph')) {
             $img_path = Storage::disk('public')->put('upload/img', $data['photograph']);
             $data["photograph"] = $img_path;
         }
 
+        /**
+         * Se nella request hai il file 'CV' storala nel disco con name public e mettilo nel percorso....
+         */
         if ($request->hasFile('CV')) {
             $file_path = Storage::disk('public')->put('upload/cv', $data['CV']);
             $data["CV"] = $file_path;
@@ -182,7 +241,7 @@ class ChefController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Rimuovi la specifica risorsa dallo store
      */
     public function destroy(Chef $chef)
     {
--- app/Http/Controllers/Api/ChefController.php
+++ app/Http/Controllers/Api/ChefController.php
@@ -3,36 +3,37 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreChefRequest;
-use App\Http\Requests\UpdateChefRequest;
 use App\Models\Chef;
-use App\Models\Review;
-use App\Models\Specialization;
-use App\Models\Sponsorship;
-use App\Models\User;
 use App\Models\Vote;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
 
 class ChefController extends Controller
 {
     public function index()
     {
         //RITORNA UN JSON CON X COSE
-        $chefs = Chef::with('user', 'sponsorships', 'specializations', 'votes', 'reviews')->withCount('reviews')
+        $chefs = Chef::with('user', 'sponsorships', 'specializations', 'votes', 'reviews')->withCount('reviews') // Con conteggio di rewievs
+            /**
+             * Aggiunge la colonna personalizzata is_sponsored per la query e crea una subquery per restituire un valore booleano
+             */
             ->addSelect([
                 'is_sponsored' => function ($query) {
-                    $query->select(DB::raw('IF(COUNT(*) > 0, 1, 0)'))
-                        ->from('chef_sponsorship')
-                        ->whereColumn('chef_sponsorship.chef_id', 'chefs.id')
-                        ->where('chef_sponsorship.start_date', '<=', now())
-                        ->where('chef_sponsorship.end_date', '>=', now());
+                    $query->select(DB::raw('IF(COUNT(*) > 0, 1, 0)')) // true se ha almeno una sponsorizzazione
+                        ->from('chef_sponsorship') // Cerca nella tabella chef_sponsorship
+                        ->whereColumn('chef_sponsorship.chef_id', 'chefs.id') // Cerca l'id dello chef della tabella chefs nella tabella chef_sponsorship
+                        ->where('chef_sponsorship.start_date', '<=', now()) // Verifica che la data di inzio sia nel passato o presente
+                        ->where('chef_sponsorship.end_date', '>=', now());  // Verifica che la data di fine sia nel futuro o presente
                 }
             ])
-            ->orderByDesc('is_sponsored')
+            ->orderByDesc('is_sponsored') // Ordina per sponsorizzato
             ->get();
+
+
+        /**
+         * Per ogni chef controlla se ha una foto o un cv
+         * se ce l'ha crea un URL pubblico
+         */
         $chefs->each(function ($chef) {
             if ($chef->photograph) {
                 $chef->photograph = asset('storage/' . $chef->photograph);
@@ -54,22 +55,32 @@ class ChefController extends Controller
     {
         $chef = Chef::with('user', 'sponsorships', 'specializations', 'votes', 'messages', 'reviews')
 
+            /**
+             * Aggiunge la colonna personalizzata average_vote alla query e crea una subquery per restituire la media del voto
+             */
             ->addSelect([
                 'average_vote' => Vote::select(DB::raw('AVG(votes.vote)'))
                     ->join('chef_vote', 'votes.id', '=', 'chef_vote.vote_id')
                     ->whereColumn('chef_vote.chef_id', 'chefs.id')
             ])
+            /**
+             * Aggiunge la colonna personalizzata is_sponsored per la query e crea una subquery per restituire un valore booleano
+             */
             ->addSelect([
                 'is_sponsored' => function ($query) {
-                    $query->select(DB::raw('IF(COUNT(*) > 0, 1, 0)'))
-                        ->from('chef_sponsorship')
-                        ->whereColumn('chef_sponsorship.chef_id', 'chefs.id')
-                        ->where('chef_sponsorship.start_date', '<=', now())
-                        ->where('chef_sponsorship.end_date', '>=', now());
+                    $query->select(DB::raw('IF(COUNT(*) > 0, 1, 0)')) // true se ha almeno una sponsorizzazione
+                        ->from('chef_sponsorship') // Cerca nella tabella chef_sponsorship
+                        ->whereColumn('chef_sponsorship.chef_id', 'chefs.id') // Cerca l'id dello chef della tabella chefs nella tabella chef_sponsorship
+                        ->where('chef_sponsorship.start_date', '<=', now()) // Verifica che la data di inzio sia nel passato o presente
+                        ->where('chef_sponsorship.end_date', '>=', now());  // Verifica che la data di fine sia nel futuro o presente
                 }
             ])
             ->find($chef->id);
 
+        /**
+         * Controlla se lo chef ha una foto o un cv
+         * se ce l'ha crea un URL pubblico
+         */
         if ($chef->photograph) {
             $chef->photograph = asset('storage/' . $chef->photograph);
         }
@@ -84,89 +95,50 @@ class ChefController extends Controller
             ]
         );
     }
-    // public function store(StoreChefRequest $request){
-    //     $email = session('user_email');
-    //     $userId = User::where('email', $email)->firstOrFail()->id;
-    //     $data = $request->validated();
-    //     $data['user_id'] = $userId;
-    //     if($request->hasFile('photograph')){
-    //         $img_path = Storage::disk('public')->put('upload/img', $data['photograph']);
-    //         $data["photograph"] = $img_path;
-    //     }
-    //     if($request->hasFile('CV')){
-    //         $file_path = Storage::disk('public')->put('upload/cv', $data['CV']);
-    //         $data["CV"] = $file_path;
-    //     }
-    //     $newChef = Chef::create($data);
-    //     $newChef->specializations()->sync($data['specializations']);
-    //     $newChef->loadMissing('user', 'specializations');
-    //     return response()->json(
-    //         [
-    //             "success" => true,
-    //             "results" => $newChef
-    //         ]);
-    // }
-    //     public function update(UpdateChefRequest $request, Chef $chef){
-    //         $data = $request->validated();
-
-    //         // Se nella request hai il file 'photograph' manda avanti la modifica. Altrimenti non fare nulla.
-    //         if($request->hasFile('photograph')){
-    //             if ($chef->photograph) {
-    //                 Storage::disk('public')->delete($chef->photograph);
-    //             }
-    //             $img_path = Storage::disk('public')->put('upload/img', $data['photograph']);
-    //             $data["photograph"] = $img_path;
-    //         }
-
-    //         if($request->hasFile('CV')){
-    //             if ($chef->CV) {
-    //                 Storage::disk('public')->delete($chef->CV);
-    //             }
-    //             $file_path = Storage::disk('public')->put('upload/cv', $data['CV']);
-    //             $data["CV"] = $file_path;
-    //         }
-
-    //          $chef->update($data);
-
-    // //       Parentesi relazione. Senza parentesi chiamo il model
-    //          $chef->specializations()->sync($data['specializations']);
-
-    //     $chef->loadMissing('specializations');
-    //     return response()->json(
-    //         [
-    //             "success" => true,
-    //             "results" => $chef
-    //         ]
-    //     );
-    // }
-
 
     public function SpecializationSearch(Request $request)
     {
-        // Get the specialization IDs from the request data
+        /**
+         * Prendi lo specialization id dalla request di default sarà un array vuoto per gestire query con 0 specializzazioni
+         */
         $specializationIds = $request->input('id', []);
+        /**
+         * Se non è un array trasformacelo
+         * Serve per quando si invia una specializzazione e mantenere logia dell'array
+         */
         if (!is_array($specializationIds)) {
             $specializationIds = [$specializationIds];
         }
         $vote = $request->input('vote');
         $reviews = $request->input('reviews');
 
-        // Start building the query
+        // Inizio a costuire la query
         $chefs = Chef::with('user', 'sponsorships', 'specializations', 'votes', 'reviews')
+
+            // Con conteggio di rewievs
             ->withCount('reviews')
-            // Join the sponsorships table to order by sponsorships.id
+
+            /**
+             * la query aggiunge la tabella sponsorships tramite le join per poter ordinare gli chef in base alla sponsorizzazione,
+             * mostrando per primi quelli con le sponsorizzazioni più recenti.
+             */
             ->leftJoin('chef_sponsorship', 'chefs.id', '=', 'chef_sponsorship.chef_id')
             ->leftJoin('sponsorships', 'chef_sponsorship.sponsorship_id', '=', 'sponsorships.id')
             ->orderBy('sponsorships.id', 'desc');
 
-        // Add a subquery to calculate the average vote
-        $chefs = $chefs->addSelect([
-            'average_vote' => Vote::select(DB::raw('AVG(votes.vote)'))
-                ->join('chef_vote', 'votes.id', '=', 'chef_vote.vote_id')
-                ->whereColumn('chef_vote.chef_id', 'chefs.id')
-        ]);
+        /**
+         * Aggiunge la colonna personalizzata average_vote alla query e crea una subquery per restituire la media del voto
+         */
+        $chefs = $chefs
+            ->addSelect([
+                'average_vote' => Vote::select(DB::raw('AVG(votes.vote)'))
+                    ->join('chef_vote', 'votes.id', '=', 'chef_vote.vote_id')
+                    ->whereColumn('chef_vote.chef_id', 'chefs.id')
+            ]);
 
-
+        /**
+         * Aggiunge la colonna personalizzata is_sponsored per la query e crea una subquery per restituire un valore booleano
+         */
         $chefs = $chefs->addSelect([
             'is_sponsored' => function ($query) {
                 $query->select(DB::raw('COUNT(*) > 0'))
@@ -185,12 +157,12 @@ class ChefController extends Controller
             });
         }
 
-        // Apply filter for average vote if present
+        // Applica il filtro per voto se presente
         if (!empty($vote)) {
-            $chefs = $chefs->having('average_vote', '>=', $vote);
+            $chefs = $chefs->having('average_vote', '>=', $vote * 2 );
         }
 
-        // Apply filter for the number of reviews if present
+        // Applica filtro per recensioni se presente
         if (!empty($reviews)) {
             $chefs = $chefs->having('reviews_count', '>=', $reviews);
         }
@@ -199,9 +171,15 @@ class ChefController extends Controller
         // Ordina i risultati mettendo prima i profili sponsorizzati
         $chefs = $chefs->orderByDesc('is_sponsored');
 
-        // Esegui la query
 
+        // Esegui la query
         $chefs = $chefs->get();
+
+
+        /**
+         * Per ogni chef controlla se ha una foto o un cv
+         * se ce l'ha crea un URL pubblico
+         */
         $chefs->each(function ($chef) {
             if ($chef->photograph) {
                 $chef->photograph = asset('storage/' . $chef->photograph);
@@ -211,16 +189,10 @@ class ChefController extends Controller
             }
         });
 
-        // Return the results in the JSON response
         return response()->json([
             'success' => true,
             'results' => $chefs
         ]);
     }
 
-
-    public function searchSpecializationHome(Request $request)
-    {
-        $specializationIds = $request->input('id');
-    }
 }
